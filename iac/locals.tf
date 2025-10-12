47ee9453e08d6c3db0ee7baf21e539eb8329fb5e
k.aasan <k.aasan@sportradar.com>
2025-10-12 21:11:27 +0200

    manual review

--- iac/locals.tf
+++ iac/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  stack_name   = "${var.app_name}-${var.environment}"
+  stack_name = "${var.app_name}-${var.environment}"
   default_tags = merge({
     Application = var.app_name
     Environment = var.environment
