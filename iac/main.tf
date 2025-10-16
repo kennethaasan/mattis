2e3f4e897e893547b91da721a1cb7927bbc21656
k.aasan <k.aasan@sportradar.com>
2025-10-16 13:37:14 +0200

    lwa fix

--- iac/main.tf
+++ iac/main.tf
@@ -82,12 +82,12 @@ module "fn" {
   environment_variables = merge(
     var.lambda_environment,
     {
-      AWS_LAMBDA_EXEC_WRAPPER = "/opt/bootstrap"
-      AWS_LWA_ASYNC_INIT      = "true"
-      AWS_LWA_PORT            = "3000"
-      DATABASE_URL            = local.database_url
-      NODE_ENV                = "production"
-      PORT                    = "3000"
+      AWS_LAMBDA_EXEC_WRAPPER    = "/opt/bootstrap"
+      AWS_LWA_ASYNC_INIT         = "true"
+      AWS_LWA_ENABLE_COMPRESSION = "true"
+      DATABASE_URL               = local.database_url
+      NODE_ENV                   = "production"
+      PORT                       = "3000"
     }
   )
 
