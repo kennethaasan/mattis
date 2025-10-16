3d93857ac303f451626228f7d5aadd861b36c3b1
Kenneth Aasan <k.aasan@sportradar.com>
2025-10-16 14:06:58 +0200

    fix: adjust lambda package preparation

--- iac/variables.tf
+++ iac/variables.tf
@@ -72,7 +72,7 @@ variable "lambda_runtime" {
 variable "lambda_handler" {
   description = "Handler for the Lambda function"
   type        = string
-  default     = "index.handler"
+  default     = "run.sh"
 }
 
 variable "lambda_memory_size" {
